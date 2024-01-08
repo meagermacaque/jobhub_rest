d391b272a0cd47cf3e70a7cf40c9704eb3aff529
Hieu Vu <vutrhieu@gmail.com>
2024-01-08 16:22:29 +0700

    2nd

--- models/Job.js
+++ models/Job.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const JobSchema = new mongoose.Schema({
     title: {type: String, required: true},
     location: {type: String, required: true},
+    company:  {type: String, required: true},
     description: {type: String, required: true},
     agentName: {type: String, required: true},
     salary: {type: String, required: true},
+    contract: {type: String, required: true},
     period: {type: String, required: true},
     hiring: {type: Boolean, required: true, default: true},
     requirements: {type: Array, required: true},
