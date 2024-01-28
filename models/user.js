e086f1cdd1c477ff133dce10db64ad0fa3ebb0b9
Hieu Vu <vutrhieu@gmail.com>
2024-01-28 19:36:41 +0700

    2nd

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    location: {type: String, required: false},
+    phone: {type: String, required: false},
+    updated: {type: Boolean, default: false},    
+    isAdmin: {type: Boolean, default: false},
+    isAgent: {type: Boolean, default: false},
+    skills:{type:Boolean, default: false, required: false},
+    profiles:{type:String, required: true, default: 'https://d326fntlu7tb1e.cloudfront.net/uploads/4821d814-ac87-4b22-aa80-ac7336916c9a-403017_avatar_default_head_person_unknown_icon.png'},
+        
+}, {timestamps: true});
+
+module.exports = mongoose.model('User', UserSchema);
